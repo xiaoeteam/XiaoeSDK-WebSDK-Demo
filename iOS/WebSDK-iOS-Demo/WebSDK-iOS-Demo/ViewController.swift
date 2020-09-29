98e206c0bdb7184518755e3156b75350dd61934a
Jacky <jackyliang@xiaoe-tech.com>
2020-09-29 16:20:40 +0800

    opt ：更新iOS样例工程

--- iOS/WebSDK-iOS-Demo/WebSDK-iOS-Demo/ViewController.swift
+++ iOS/WebSDK-iOS-Demo/WebSDK-iOS-Demo/ViewController.swift
@@ -13,8 +13,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.white
+        
+        //测试按钮
+        let button = UIButton(frame: CGRect(x: 10, y: 100, width: 200, height: 50))
+        button.setTitle("进入小鹅H5页面", for: UIControl.State.normal)
+        button.setTitleColor(UIColor.white, for: UIControl.State.normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        button.backgroundColor = UIColor.blue
+        button.center = view.center
+        view.addSubview(button)
+        button.addTarget(self, action: #selector(goToWebView), for: UIControl.Event.touchUpInside)
+        
     }
     
+    @objc func goToWebView() -> Void {
+        let webViewController = WebViewController()
+        webViewController.loadUrl = "https://appxrwbvfhb8064.h5.xiaoeknow.com/v1/course/alive/l_5f72e59be4b0e95a89c1be7f?type=2"
+        self.navigationController?.pushViewController(webViewController, animated: true)
+    }
 
 }
 
--- iOS/WebSDK-iOS-Demo/WebSDK-iOS-Demo/WebViewController.swift
+++ iOS/WebSDK-iOS-Demo/WebSDK-iOS-Demo/WebViewController.swift
@@ -19,6 +19,7 @@ class WebViewController : UIViewController {
         return view
     }()
     
+    var loadUrl:String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,11 +28,39 @@ class WebViewController : UIViewController {
         //添加webView
         view.addSubview(webView)
         
+        //加载网页
+        if let urlstr = loadUrl , let url = URL(string: urlstr) {
+            webView.load(URLRequest(url: url))
+        }
         
+    }
+    
+    //自定义的加载视图（展示）
+    func showLoading() -> Void {
+        var content = VHUDContent(.loop(3.0))
+        content.shape = .circle
+        content.style = .light
+        VHUD.show(content)
+    }
+    
+    //自定义的加载视图（隐藏）
+    func hideLoading() -> Void {
+        VHUD.dismiss(1.0, 1.0)
     }
 }
 
 //网页代理回调
 extension WebViewController : WKUIDelegate,WKNavigationDelegate {
     
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        showLoading()
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        hideLoading()
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        hideLoading()
+    }
 }
