476915e8465b8e1177493a19c4d55665e915e881
Jacky <jackyliang@xiaoe-tech.com>
2020-09-29 14:56:02 +0800

    opt ：更新iOS样例工程

--- iOS/WebSDK-iOS-Demo/WebSDK-iOS-Demo/WebViewController.swift
+++ iOS/WebSDK-iOS-Demo/WebSDK-iOS-Demo/WebViewController.swift
@@ -8,11 +8,30 @@
 
 import Foundation
 import UIKit
+import WebKit
 
 class WebViewController : UIViewController {
+    
+    lazy var webView : WKWebView = {
+        let view = WKWebView.init(frame: self.view.bounds)
+        view.uiDelegate = self;
+        view.navigationDelegate = self;
+        return view
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.white
+        
+        //添加webView
+        view.addSubview(webView)
+        
+        
     }
+}
+
+//网页代理回调
+extension WebViewController : WKUIDelegate,WKNavigationDelegate {
+    
 }
